8760e0423da5eb58b11c63cf89c9cae27811ff21
merryiscat <minhyuk1463@naver.com>
2025-08-01 09:20:27 +0900

    의도분류테스트진행

--- mcp_study_react/app.py
+++ mcp_study_react/app.py
@@ -12,7 +12,7 @@ load_dotenv()
 graph_app = Project_Graph()
 
 result = graph_app.invoke({
-    "user_input": "아 피곤하다",
+    "user_input": "신논현 역 주변에서 저녁을 먹을껀데, 진짜 인생 맛집을 찾고 싶어어",
     "thread_id": "run-0001" 
 })
 
