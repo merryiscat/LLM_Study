1fd0e2550a26eed7cbb7509d46965c49077dde4d
merryiscat <minhyuk1463@naver.com>
2025-08-12 16:37:37 +0900

    노드정리

--- mcp_study_react/chain/__init__.py
+++ mcp_study_react/chain/__init__.py
@@ -1,6 +1,7 @@
 from .identity_chain import identity_chain
 from .intent_classify_chain import intent_classify_chain
 from .intent_extract_chain import intent_extract_chain
+from .talk_chain import talk_chain
 
 __all__ = [
     "identity_chain",
